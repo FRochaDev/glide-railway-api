7f0085089dfa76912f755f3f799a10e2dc94e04c
Flávio Rocha <flaviobgrocha@gmail.com>
2026-06-05 16:29:56 +0100

    Convert server to ES modules

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const express = require("express");
-const cors = require("cors");
+import express from "express";
+import cors from "cors";
+import * as glide from "@glideapps/tables";
 
 const app = express();
 
@@ -8,20 +9,39 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
+const gratefulPenTable = glide.table({
+  token: process.env.GLIDE_TOKEN,
+  app: "hscl4V0VoiMpK9mxpx17",
+  table: "native-table-SA33qmMpIzkczl6DMc0e",
+  columns: {
+    inputNumber: { type: "number", name: "Name" },
+    response: { type: "string", name: "xmPst" },
+    status: { type: "string", name: "4l7aC" }
+  }
+});
+
 app.get("/", (req, res) => {
   res.json({
     status: "online"
   });
 });
 
-app.post("/multiply", (req, res) => {
-
+app.post("/multiply", async (req, res) => {
   const value = Number(req.body.value || 0);
+  const result = value * 2;
 
-  res.json({
-    result: value * 2
+  const rowId = await gratefulPenTable.add({
+    inputNumber: value,
+    response: JSON.stringify({
+      result
+    }),
+    status: "Done"
   });
 
+  res.json({
+    result,
+    rowId
+  });
 });
 
 app.listen(PORT, () => {
